bd8c3f3af254a8ac3a04a662cbf60675ee6ec1a0
naveshiki0 <kounanataiti@icloud.com>
2025-07-05 23:43:31 +0900

    ヒーローとフッターお問い合わせの改修以外レビュー前完成

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Footer() {
+  const pathname = usePathname();
+  const isContactPage = pathname === "/contact";
+
   return (
     <footer className="bg-white  text-white pt-5">
       {/* <section className="relative h-[400px] ">
@@ -29,23 +34,27 @@ export default function Footer() {
         </div>
       </section> */}
 
-      <section className=" py-24">
-        <div className="max-w-6xl mx-auto px-8">
-          <div className="text-3xl text-blue-800 font-black mb-6">Contact</div>
-          <h2 className="text-xl md:text-3xl font-black text-gray-900 mb-6">
-            ご相談・ご応募はこちらから
-          </h2>
-          <p className="text-xl md:text-3xl font-semibold text-gray-800 mb-8 whitespace-nowrap">
-            エトワールに関心のある方は、ぜひお気軽にご連絡ください。
-          </p>
-          <Link
-            href="/contact"
-            className="inline-block bg-black text-white text-lg px-8 py-4 rounded-full shadow-md hover:bg-gray-800 transition"
-          >
-            お問い合わせフォームへ →
-          </Link>
-        </div>
-      </section>
+      {!isContactPage && (
+        <section className="py-24">
+          <div className="max-w-6xl mx-auto px-8">
+            <div className="text-3xl text-blue-800 font-black mb-6">
+              Contact
+            </div>
+            <h2 className="text-xl md:text-3xl font-black text-gray-900 mb-6">
+              ご相談・ご応募はこちらから
+            </h2>
+            <p className="text-xl md:text-3xl font-semibold text-gray-800 mb-8 whitespace-nowrap">
+              エトワールに関心のある方は、ぜひお気軽にご連絡ください。
+            </p>
+            <Link
+              href="/contact"
+              className="inline-block bg-black text-white text-lg px-8 py-4 rounded-full shadow-md hover:bg-gray-800 transition"
+            >
+              お問い合わせフォームへ →
+            </Link>
+          </div>
+        </section>
+      )}
       <div className=" bg-gray-900 w-full px-6 py-12 grid gap-10 grid-cols-1 ">
         <div className="flex flex-col items-center space-y-6 text-center">
           <ul className="flex flex-wrap justify-center gap-x-6">
